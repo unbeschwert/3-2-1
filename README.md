b3647809fc8d63b39a1b84d4283c2af16ae8db1c
Nirmal Kumar Ramadoss <nirmal.kumar.3.14159@gmail.com>
2023-10-21 17:43:25 +0200

    Incorporates feedback from @kpcyrd

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Introduction
 I really enjoy the [3-2-1](https://jamesclear.com/3-2-1) newsletter from [James Clear](https://jamesclear.com/). Especially, the questions that come at the end. It has often helped me take a minute and think about things at a macro level. This repository is a simple rust program that scraps the newsletter and extracts these questions to a file. 
 
+## Update
+Huge thanks to [feedback](https://gist.github.com/kpcyrd/7342c3f833fbd09e98a765ca8417922e) from [kpcyrd](https://github.com/kpcyrd). I learned about the [anyhow](https://docs.rs/anyhow/latest/anyhow/) crate from them and have incorporated the suggested changes.
+
 # Instructions
 The following commands can be used to generate the *questions* file:
 ```
@@ -10,7 +13,7 @@ cargo build
 ./target/debug/james-clear-3-2-1
 ```
 
-The file is can be found in ```$HOME/questions```.
+The file can be found at ```$HOME/questions```.
 
 Further in Linux, one can do the following: Add ```alias question="cat $HOME/questions | shuf | head -1"``` to ```.bash_aliases``` and enjoy pondering about the questions.
 
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,71 @@
 use std::{fs, io::Write};
 
+use anyhow::{Context, Result};
 use dirs::home_dir;
 use scraper::{Element, Html, Selector};
 
-static URL: &str = "https://jamesclear.com/3-2-1";
+const URL: &str = "https://jamesclear.com/3-2-1";
+const H2_TAG_TEXT: &str = "1 QUESTION FOR YOU";
+const P_TAG: &str = "p";
 
-fn get_questions(doc: &Html, sel: &Selector) -> String {
-    let stopholder: &str = "p";
-    let h2_text = "1 QUESTION FOR YOU";
-    let mut questions = String::new();
-    let mut valid: bool = true;
-
+fn get_questions(doc: &Html, sel: &Selector) -> Result<Option<String>> {
     for h2_element in doc.select(&sel) {
-        if h2_element.text().collect::<String>() == h2_text {
-            let mut p_element = match h2_element.next_sibling_element() {
-                Some(x) => x,
-                None => panic!("{:#?}", doc),
-            };
-            while valid {
-                if p_element.value().name() == stopholder {
-                    let text = p_element.text().collect::<String>();
-                    if text.starts_with("Until") {
-                        break;
-                    } else {
-                        questions.push_str(&text);
-                        questions.push(' ');
-                        p_element = match p_element.next_sibling_element() {
-                            Some(x) => x,
-                            None => break,
-                        }
-                    }
+        if h2_element.text().collect::<String>() == H2_TAG_TEXT {
+            let mut p_element = h2_element
+                .next_sibling_element()
+                .with_context(|| format!("Couldn't find the question! {doc:#?}"))?;
+            let mut questions = String::new();
+            while p_element.value().name() == P_TAG {
+                let text = p_element.text().collect::<String>();
+                if text.starts_with("Until") {
+                    break;
                 } else {
-                    valid = false;
+                    questions.push_str(&text);
+                    questions.push(' ');
+                    if let Some(next_element) = p_element.next_sibling_element() {
+                        p_element = next_element;
+                    } else {
+                        break;
+                    }
                 }
             }
+            return Ok(Some(questions));
         }
     }
-
-    questions
+    Ok(None)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_name = home_dir().unwrap().join("questions");
-    let mut file_obj = fs::File::create(file_name).expect("Unable to create file");
-    let main_page = reqwest::blocking::get(URL)?.text()?;
+fn fetch_html_doc(url: &str) -> Result<String> {
+    let text = reqwest::blocking::get(url)
+        .with_context(|| format!("Failed to send http request: {url:?}"))?
+        .error_for_status()
+        .with_context(|| format!("Failed to download: {url:?}"))?
+        .text()
+        .context("Failed to read http response")?;
+    Ok(text)
+}
+
+fn main() -> Result<()> {
+    let file_name = home_dir()
+        .context("Couldn't determine home directory")?
+        .join("questions");
+    let mut file_obj = fs::File::create(&file_name)
+        .with_context(|| format!("Couldn't create file {file_name:?}"))?;
+    let main_page = fetch_html_doc(&URL)?;
     let main_html = Html::parse_document(&main_page);
     let a_selector = Selector::parse(r#"a[class="all-articles__news__post"]"#).unwrap();
 
     for element in main_html.select(&a_selector) {
         if let Some(newsletter_url) = element.value().attr("href") {
-            let newsletter = reqwest::blocking::get(newsletter_url)?.text()?;
+            let newsletter = fetch_html_doc(&newsletter_url)?;
             let newsletter_html = Html::parse_document(&newsletter);
             let h2_selector = Selector::parse("h2").unwrap();
-            let questions = get_questions(&newsletter_html, &h2_selector);
-            if !questions.is_empty() {
+            if let Some(mut questions) = get_questions(&newsletter_html, &h2_selector)? {
+                questions.push('\n');
                 file_obj
                     .write_all(questions.as_bytes())
                     .expect("Unable to write to file");
             }
-            file_obj
-                .write_all("\n".as_bytes())
-                .expect("Unable to write to file");
         };
     }
     Ok(())
